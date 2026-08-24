18df041936d41ce46f6de849ae6bfa1552fa28b3
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-08-24 11:41:59 +0500

    today, learnt about post request, pydantic, mabse model

--- database.py
+++ database.py
@@ -1,2 +1,4 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 engine = create_engine("sqlite:///blog.db")
+sessionLocal = sessionmaker(bind=engine)
---
+++ main_api.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Depends, HTTPException
+from database import sessionLocal
+from sqlalchemy.orm import Session
+from models import User
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class UserCreate(BaseModel):
+    name: str
+
+@app.get("/users/{user_id}")
+def get_user(user_id:int, db: Session = Depends(get_db) )->dict:
+    user = db.get(User,user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail='User Not found')
+    else:
+        return {'user_name' : user.name,
+                'user_id' : user.id
+                }
+
+@app.post("/users")
+def create_user(user: UserCreate, db: Session = Depends(get_db))->dict:
+    new_user = User(
+        name = user.name,
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {
+        'status': "User is successfully created"
+    }
